7713f883505a52695d4c7ab56c1e70b0d266239c
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2026-09-11 11:00:26 +0700

    feat: Update project action for create, update and delete

--- actions/project.ts
+++ actions/project.ts
@@ -1,10 +1,81 @@
 import { requiredProjectMember } from "@/lib/permissions";
+import { prisma } from "@/lib/prisma";
+import { checkRateLimit } from "@/lib/ratelimit";
+import { requireSession } from "@/lib/session";
 import { ProjectActionState } from "@/types/project";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import z from "zod";
+
+const CreateProjectSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  description: z.string().optional(),
+  color: z.string().optional(),
+});
+
+export async function createProject(
+  _prev: ProjectActionState,
+  formData: FormData,
+): Promise<ProjectActionState> {
+  // 1. Authentication
+  const session = await requireSession();
+
+  // 2. Rate Limiter
+  const { limited } = await checkRateLimit(`createProject:${session.user.id}`);
+  if (limited) return { error: "Too many requests. Please wait" };
+
+  // 3. Validasi
+  const parsed = CreateProjectSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description") || undefined,
+    color: formData.get("color") || undefined,
+  });
+  if (!parsed.success) return { error: parsed.error.issues[0].message };
+
+  // 4. Execute data
+  const project = await prisma.project.create({
+    data: {
+      ...parsed.data,
+      ownerId: session.user.id,
+      members: { create: { userId: session.user.id } },
+    },
+  });
+
+  revalidatePath("/projects");
+  redirect(`/projects/${project.id}`);
+}
 
 export async function updateProject(
   projectId: string,
   _prev: ProjectActionState,
   formData: FormData,
-) {
-  await requiredProjectMember(projectId)
+): Promise<ProjectActionState> {
+  await requiredProjectMember(projectId);
+
+  const parsed = CreateProjectSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description") || undefined,
+    color: formData.get("color") || undefined,
+  });
+  if (!parsed.success) return { error: parsed.error.issues[0].message };
+
+  await prisma.project.update({
+    where: { id: projectId },
+    data: parsed.data,
+  });
+
+  revalidatePath(`/projects/${projectId}`);
+  redirect(`/projects/${projectId}`);
+}
+
+export async function deleteProject(projectId: string): Promise<void> {
+  const session = await requireSession();
+  const project = await prisma.project.findUnique({ where: { id: projectId } });
+
+  if (!project || project.ownerId !== session.user.id) return;
+
+  await prisma.project.delete({ where: { id: projectId } });
+
+  revalidatePath("/projects");
+  redirect("/projects");
 }
